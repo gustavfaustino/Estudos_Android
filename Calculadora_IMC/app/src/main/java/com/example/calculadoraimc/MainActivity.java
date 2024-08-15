2e045c8212d3a1cbc91267f1b4b075f5508eec70
gustavfaustino <gustav.faustino107@gmail.com>
2024-08-15 11:10:51 -0300

    Pequenos ajustes no Layout da calculadora

--- Calculadora_IMC/app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ Calculadora_IMC/app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -20,5 +20,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
